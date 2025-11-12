f9683181a4ac6db3e54dc705ce171cc3c0c43caa
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2025-11-13 00:19:48 +0530

    Time: 19 ms (41.75%), Space: 97.8 MB (15.42%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>>res = new ArrayList<>();
+        List<Integer>comb = new ArrayList<>();
+        back(1,comb,res,n,k);
+        return res;
+    }
+    private void back(int start,List<Integer>comb,List<List<Integer>> res,int n,int k){
+        if(comb.size()==k){
+            res.add(new ArrayList<>(comb));
+            return;
+        }
+        for(int i=start;i<=n;i++){
+            comb.add(i);
+            back(i+1,comb,res,n,k);
+            comb.remove(comb.size()-1);
+        }
+
+    }
+}

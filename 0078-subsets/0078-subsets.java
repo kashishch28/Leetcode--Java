43f9135d033a3031c5a7dd64f74ddc173a01c19a
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2025-11-13 11:02:22 +0530

    Time: 1 ms (55.32%), Space: 44.2 MB (9.16%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,31 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        int n=nums.length;
-        int subset = 1<<n;
-        for(int i=0;i<subset;i++){
-            List<Integer> sub = new ArrayList<>();
-            for(int j=0;j<n;j++){
-                if((i & (1<<j))!=0){
-                  sub.add(nums[j]);  
-                }
-            }
-            result.add(sub);
+        back(0,nums,new ArrayList<>(),result);
+        return result;    
+    }
+    private void back(int ind,int[] nums,List<Integer>temp,List<List<Integer>>ans){
+        ans.add(new ArrayList<>(temp));
+        for(int i=ind;i<nums.length;i++){
+            temp.add(nums[i]);
+            back(i+1,nums,temp,ans);
+            temp.remove(temp.size()-1);
         }
-        return result;
     }
 }
+
+
+
+// List<List<Integer>> result = new ArrayList<>();
+        // int n=nums.length;
+        // int subset = 1<<n;
+        // for(int i=0;i<subset;i++){
+        //     List<Integer> sub = new ArrayList<>();
+        //     for(int j=0;j<n;j++){
+        //         if((i & (1<<j))!=0){
+        //           sub.add(nums[j]);  
+        //         }
+        //     }
+        //     result.add(sub);
+        // }
+        // return result;

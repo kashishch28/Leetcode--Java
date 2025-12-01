2342f77ce31c6d76512eda1c119892aa03bf40cd
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2025-12-01 23:38:26 +0530

    Time: 3 ms (21.03%), Space: 41.8 MB (86.62%) - LeetHub

---
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if(k==2)return -1;
+        if(k==5)return -1;
+        int rem=0;
+        for(int i=1;i<=k;i++){
+            rem = (rem*10+1)%k;
+            if(rem==0)return i;
+        }
+        return -1;
+    }
+}

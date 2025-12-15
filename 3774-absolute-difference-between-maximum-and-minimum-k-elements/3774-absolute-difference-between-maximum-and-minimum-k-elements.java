c37530593097325907b72a8df6510f825b2028c2
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2025-12-15 22:22:31 +0530

    Time: 7 ms (0%), Space: 46.7 MB (100%) - LeetHub

---
+++ 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int absDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int min=0;
+        int max=0;
+        for(int i=0;i<k;i++){
+            min+=nums[i];
+        }
+        for(int i=nums.length-1;i>=nums.length-k;i--){
+            max+=nums[i];
+        }
+        return Math.abs(max-min);
+    }
+}

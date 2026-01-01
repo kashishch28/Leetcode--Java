614e45b2d1c73d3953a3d3d18a20f8c8ebdabc81
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2026-01-02 00:03:51 +0530

    Update 0066-plus-one.java

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] plusOne(int[] digits) {
         
         for(int i =digits.length-1;i>=0;i--){
-            if(digits[i]<9){
-                ++digits[i];
+            if(digits[i]<9){ //rightmost digit is not 9 for carry
+                ++digits[i];//plus 1 and return
                 return digits;
             }
-            digits[i]=0;
+            digits[i]=0;//rightmost is 9 plus one and last one became 0 and loop runs 
         }
-            int [] ans = new int[digits.length+1];
-            ans[0] = 1;
+            int [] ans = new int[digits.length+1];//all digits are 9 increase array length
+            ans[0] = 1; //add 1 in index 0 and all rest automatically chaged to 0
             return ans;
 
         

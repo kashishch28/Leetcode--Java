4a71ef23e8469f667a3ea9e2e3d93b4422916b66
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2026-02-07 20:54:25 +0530

    Time: 18 ms (100%), Space: 47.9 MB (30.43%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public int minimumDeletions(String s) {
+//         int dp = 0;
+//         int countB = 0;
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+//             if (c == 'a') {
+//                 dp = Math.min(dp + 1, countB);
+//             } else {
+//                 countB++;
+//             }
+//         }
+
+//         return dp;
+//     }
+// }
+
 class Solution {
     public int minimumDeletions(String s) {
-        int dp = 0;
-        int countB = 0;
+        int ans = 0;
+        int count = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == 'a') {
-                dp = Math.min(dp + 1, countB);
-            } else {
-                countB++;
+        for(char ch: s.toCharArray()) {
+            if(ch == 'b') {
+                count += 1;
+            } else if(count > 0) {
+                ans+= 1;
+                count -= 1;
+             }
             }
-        }
-
-        return dp;
+           
+           return ans;
+        } 
     }
-}

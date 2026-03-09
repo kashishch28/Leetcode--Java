76123307550f13be3847ecaa07332ca3c03b3d4a
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2026-03-09 23:04:53 +0530

    Time: 24 ms (77.42%), Space: 48.4 MB (96.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,15 +1,35 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int c=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    c++;
-                }
+        // int c=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         if(sum==k){
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
+
+         Map<Integer, Integer> mp = new HashMap<>();// sum,count;
+        int ans = 0;
+        int sum = 0;
+        mp.put(0, 1);
+
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+
+            if (mp.containsKey(sum - k)) {
+                ans += mp.get(sum - k);
+            }
+
+            if (!mp.containsKey(sum)) {
+                mp.put(sum, 1);
+            } else {
+                mp.put(sum, mp.get(sum) + 1);
             }
         }
-        return c;
+        return ans;
     }
 }

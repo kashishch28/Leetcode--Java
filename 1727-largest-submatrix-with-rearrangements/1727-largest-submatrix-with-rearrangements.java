0ce62b6df516afaaebda0421ed4a90d58e9a7a54
Kashish Chaudhary <155078004+kashishch28@users.noreply.github.com>
2026-03-17 15:23:38 +0530

    Time: 14 ms (51.3%), Space: 114.5 MB (42.61%) - LeetHub

---
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]!=0){
+                    matrix[i][j]+=matrix[i-1][j];//consecutive 1's
+
+                }
+            }
+        }
+        int area=0;
+        for(int i=0;i<m;i++){
+            int[] row = matrix[i].clone();
+            Arrays.sort(row);
+            //descending
+            for(int j=n-1;j>=0;j--){
+                int height = row[j];
+                int width = n-j;
+                
+                area = Math.max(area,width*height);
+            }
+        }
+        return area;
+    }
+}
